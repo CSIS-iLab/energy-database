83db0f46bb37ef735ed703628c421b358a56de1b
jdromero88 <jdromerov88@gmail.com>
2022-03-01 15:47:16 -0500

    Add base structure

--- src/App.svelte
+++ src/App.svelte
@@ -1,32 +1,12 @@
 <script>
-	export let name;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<div>
+    Hello World!
+  </div>
 </main>
 
-<style type="text/scss">
+<style type="text/scss" global>
   @import './scss/layout/_base.scss';
-
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
