3423e7c175694b2e38443264d26db115d0afbc37
Serven Maraghi <maraghiserven@gmail.com>
2022-03-22 15:41:57 -0400

    Restructure and parse data

--- src/App.svelte
+++ src/App.svelte
@@ -1,46 +1,46 @@
 <script>
-  import { onMount } from 'svelte'
-  import getData from './data'
-  import Table from './components/Table.svelte'
+  import { onMount } from "svelte";
+  import getData from "./data";
+  import Table from "./components/Table.svelte";
 
-  const dataset =  {}
+  let dataset = {};
+  let filteredData = [];
 
-  onMount( async () => {
-    const res = await getData()
-    // console.log(res.data)
-    dataset["data"] = [...res.data]
-    dataset["originalData"] = [...res.data]
-    dataset["titles"] = [...res.data.titles]
-    dataset["states"] = [...res.data.states]
-    dataset["authority"] = [...res.data.authority]
-    dataset["resourceType"] = [...res.data.resourceType]
-    // console.log(dataset)
-  })
+  onMount(async () => {
+    const res = await getData();
+    dataset = res;
+    console.log(dataset);
+    filteredData = dataset.data;
+  });
 
   const filterBy = (e) => {
     // console.log(e.target)
-    const filter = e.target.value
-    const key = e.target.attributes.key.value
+    const filter = e.target.value;
+    const key = e.target.attributes.key.value;
     // console.log(filter)
     // console.log(key)
-    const result = dataset.originalData.filter( el => el[key].includes( filter ) )
+    const result = dataset.originalData.filter((el) =>
+      el[key].includes(filter)
+    );
     // console.log(result)
-    dataset.data = [...result]
-  }
+    dataset.data = [...result];
+  };
 </script>
 
 <main>
-	<div>
+  <div>
     <h1>Energy Database | SPA</h1>
-
-    <Table
-      dataset={dataset}
-      selected={filterBy}
-    />
-  </div>  
+    {#if dataset.data && dataset.data.length > 0}
+      <Table {dataset} {filteredData} selected={filterBy} />
+    {:else}
+      <div class="loading-container">
+        <div class="loading" />
+      </div>
+    {/if}
+  </div>
 </main>
 
-<style lang='scss' global>
+<style lang="scss" global>
   // @import './scss/layout/_base.scss';
-  @use './scss/main.scss';
+  @use "./scss/main.scss";
 </style>
